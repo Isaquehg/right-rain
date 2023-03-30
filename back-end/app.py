9c6fb98f8d55946ba4d78336c03e0b9bd4759a43
Isaquehg <isaqueh.goncalves@gmail.com>
2023-03-30 09:52:17 -0300

    update

--- back-end/app.py
+++ back-end/app.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI, Body, HTTPException, status
 from fastapi.responses import Response, JSONResponse
 from fastapi.encoders import jsonable_encoder
 from pydantic import BaseModel, Field, EmailStr
-from bson import ObjectId
+from bson.objectid import ObjectId
 from typing import Optional, List
 import motor.motor_asyncio
 from typing import List, Dict

d6d0752667d2363cf626b9e8745e8e91034952cc
Giv <giovanifinazzi@gmail.com>
2023-05-06 17:39:06 -0300

    NavigationDrawer and Mapbox

--- front-end/right-rainAS/app/src/main/java/com/example/rightrain/LoginActivity.java
+++ front-end/right-rainAS/app/src/main/java/com/example/rightrain/LoginActivity.java
@@ -1,6 +1,8 @@
 package com.example.rightrain;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,5 +11,11 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        Button botao_acessar = findViewById(R.id.botao_acessar);
+        // Botao acessar deve acessar a main activity
+        botao_acessar.setOnClickListener(v -> {
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(intent);
+        });
     }
 }
--- front-end/right-rainAS/app/src/main/java/com/example/rightrain/MainActivity.java
+++ front-end/right-rainAS/app/src/main/java/com/example/rightrain/MainActivity.java
@@ -3,12 +3,20 @@ package com.example.rightrain;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.widget.Button;
+
+import com.mapbox.maps.MapView;
+import com.mapbox.maps.Style;
+import com.mapbox.maps.plugin.Plugin;
 
 public class MainActivity extends AppCompatActivity {
+    MapView mapView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mapView = findViewById(R.id.mapView);
         setContentView(R.layout.activity_main);
     }
 }

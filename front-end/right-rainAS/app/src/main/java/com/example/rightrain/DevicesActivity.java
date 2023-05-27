4358aad5e01d2255d27dee0e26f6d49bc684ce48
Giv <giovanifinazzi@gmail.com>
2023-05-27 13:22:42 -0300

    Warley's design II

---
+++ front-end/right-rainAS/app/src/main/java/com/example/rightrain/DevicesActivity.java
@@ -0,0 +1,13 @@
+package com.example.rightrain;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DevicesActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_devices);
+    }
+}
--- front-end/right-rainAS/app/src/main/java/com/example/rightrain/fetchData.java
+++ front-end/right-rainAS/app/src/main/java/com/example/rightrain/fetchData.java
@@ -1,14 +1,20 @@
 package com.example.rightrain;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
 
 public class fetchData extends Thread {
+    String data = "";
+    ArrayList<String> userList;
     @Override
     public void run() {
         try {
@@ -17,8 +23,25 @@ public class fetchData extends Thread {
             InputStream is = hc.getInputStream();
             BufferedReader bf = new BufferedReader(new InputStreamReader(is));
             String line;
+
+            while((line = bf.readLine()) != null){
+                data = data + line;
+            }
+            if(!data.isEmpty()){
+                JSONObject jsonObject = new JSONObject(data);
+                JSONArray users = jsonObject.getJSONArray("user");
+                userList.clear();
+                for (int i = 0; i < users.length(); i++) {
+                    org.json.JSONObject names = users.getJSONObject(i);
+                    String name = names.getString("name");
+                    userList.add(name);
+                }
+            }
+
         } catch (IOException e) {
             e.printStackTrace();
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
         }
     }
 }

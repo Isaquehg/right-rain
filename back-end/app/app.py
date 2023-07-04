d0ea1ba4d172c3b0f54b8d517d52afdde618a475
Isaquehg <isaqueh.goncalves@gmail.com>
2023-07-04 15:01:08 -0300

    update req. Dev. Model

--- back-end/app/app.py
+++ back-end/app/app.py
@@ -39,7 +39,7 @@ class OAuth2PasswordRequestFormCustom(BaseModel):
 class DeviceData(BaseModel):
     _id: str = Field(...)
     u_id: str = Field(...)
-    d_id: str = Field(...)
+    d_id: Optional[str] = Field(None)
     latitude: Optional[float] = Field(None)
     longitude: Optional[float] = Field(None)
     date: Optional[str] = Field(None, regex=r"^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}$")
